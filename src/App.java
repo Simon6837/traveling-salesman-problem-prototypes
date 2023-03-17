e0d6d1611e5246cbd143c5462ad3ac707891063c
Simon van den Berg <simonschrotenberg@gmail.com>
2023-03-17 21:53:05 +0100

    added nearest neighbor, bruteforce and farthest insertion  methods generated with chatGPT

--- src/App.java
+++ src/App.java
@@ -1,10 +1,24 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        DistanceMatrix dm = new DistanceMatrix();
-        dm.setStart(10, 2, 41);
-        dm.setPoint1(10, 7, 4);
-        dm.setPoint2(2, 7, 11);
-        dm.setPoint3(41, 4, 11);
-        dm.printDistanceMatrix();
+        int[] productCords1 = new int[] { 1, 1 };
+        int[] productCords2 = new int[] { 1, 1 };
+        int[] productCords3 = new int[] { 1, 1 };
+
+        DistanceMatrix store = new DistanceMatrix(productCords1, productCords2, productCords3);
+
+        int[] nearestNeighbor = store.getNearestNeighborRoute();
+        int[] bruteForce = store.getBruteForceRoute();
+        int[] farthestInsertion = store.getFarthestInsertionRoute();
+        store.printDistanceMatrix();
+        System.out.println(store.toString());
+        // Print the routes
+        System.out.println("Nearest Neighbor Route:");
+        System.out.println(Arrays.toString(nearestNeighbor));
+        System.out.println("Brute Force Route:");
+        System.out.println(Arrays.toString(bruteForce));
+        System.out.println("Farthest Insertion Route:");
+        System.out.println(Arrays.toString(farthestInsertion));
     }
 }
--- src/DistanceMatrix.java
+++ src/DistanceMatrix.java
@@ -1,56 +1,242 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DistanceMatrix {
-    private int start[] = new int[4];
-    private int point1[] = new int[4];
-    private int point2[] = new int[4];
-    private int point3[] = new int[4];
+    private int[] startCoords = { 0, 0 };
+    private int[] productCords1 = new int[2];
+    private int[] productCords2 = new int[2];
+    private int[] productCords3 = new int[2];
+    private int[][] distanceMatrix = new int[4][4];
 
-    public DistanceMatrix() {
-        // Empty constructor
-    }
-    // public DistanceMatrix(int[] start, int[] point1, int[] point2, int[] point3) {
-    //     this.start = start;
-    //     this.point1 = point1;
-    //     this.point2 = point2;
-    //     this.point3 = point3;
-    // }
-
-    public void setStart(int distanceToPoint1, int distanceToPoint2, int distanceToPoint3) {
-        start[0] = 0;
-        start[1] = distanceToPoint1;
-        start[2] = distanceToPoint2;
-        start[3] = distanceToPoint3;
+    public DistanceMatrix(int[] productCords1, int[] productCords2, int[] productCords3) {
+        this.productCords1 = productCords1;
+        this.productCords2 = productCords2;
+        this.productCords3 = productCords3;
+        distanceMatrix = createDistanceMatrix();
     }
 
-    public void setPoint1(int distanceToStart, int distanceToPoint2, int distanceToPoint3) {
-        point1[0] = distanceToStart;
-        point1[1] = 0;
-        point1[2] = distanceToPoint2;
-        point1[3] = distanceToPoint3;
+    public DistanceMatrix(int product1x, int product1y, int product2x, int product2y, int product3x, int product3y) {
+        productCords1[0] = product1x;
+        productCords1[1] = product1y;
+        productCords2[0] = product2x;
+        productCords2[1] = product2y;
+        productCords3[0] = product3x;
+        productCords3[1] = product3y;
+        distanceMatrix = createDistanceMatrix();
     }
 
-    public void setPoint2(int distanceToStart, int distanceToPoint1, int distanceToPoint3) {
-        point2[0] = distanceToStart;
-        point2[1] = distanceToPoint1;
-        point2[2] = 0;
-        point2[3] = distanceToPoint3;
+    public DistanceMatrix(int[][] distanceMatrix2) {
+        distanceMatrix = distanceMatrix2;
     }
 
-    public void setPoint3(int distanceToStart, int distanceToPoint1, int distanceToPoint2) {
-        point3[0] = distanceToStart;
-        point3[1] = distanceToPoint1;
-        point3[2] = distanceToPoint2;
-        point3[3] = 0;
+    public int[][] createDistanceMatrix() {
+        int[][] matrix = new int[4][4];
+        matrix[0][0] = 0;
+        matrix[1][1] = 0;
+        matrix[2][2] = 0;
+        matrix[3][3] = 0;
+
+        matrix[0][1] = matrix[1][0] = distance(startCoords, productCords1);
+        matrix[0][2] = matrix[2][0] = distance(startCoords, productCords2);
+        matrix[0][3] = matrix[3][0] = distance(startCoords, productCords3);
+
+        matrix[1][2] = matrix[2][1] = distance(productCords1, productCords2);
+        matrix[1][3] = matrix[3][1] = distance(productCords1, productCords3);
+
+        matrix[2][3] = matrix[3][2] = distance(productCords2, productCords3);
+
+        return matrix;
     }
 
     public void printDistanceMatrix() {
-        // Print the distance matrix
-        System.out.println("Distance Matrix:");
-        System.out.println("\tStart\tPoint 1\tPoint 2\tPoint 3");
+        for (int[] row : distanceMatrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
 
-        System.out.println("Start\t" + start[0] + "\t" + start[1] + "\t" + start[2] + "\t" + start[3]);
-        System.out.println("Point 1\t" + point1[0] + "\t" + point1[1] + "\t" + point1[2] + "\t" + point1[3]);
-        System.out.println("Point 2\t" + point2[0] + "\t" + point2[1] + "\t" + point2[2] + "\t" + point2[3]);
-        System.out.println("Point 3\t" + point3[0] + "\t" + point3[1] + "\t" + point3[2] + "\t" + point3[3]);
+    private int distance(int[] a, int[] b) {
+        int dx = a[0] - b[0];
+        int dy = a[1] - b[1];
+        return (int) Math.sqrt(dx * dx + dy * dy);
+    }
 
+    public String toString() {
+        return "Start Coordinates: (" + startCoords[0] + ", " + startCoords[1] + ")\n"
+                + "Product Coordinates 1: (" + productCords1[0] + ", " + productCords1[1] + ")\n"
+                + "Product Coordinates 2: (" + productCords2[0] + ", " + productCords2[1] + ")\n"
+                + "Product Coordinates 3: (" + productCords3[0] + ", " + productCords3[1] + ")";
+    }
+
+    // nearest neighbor algorithm
+
+    public int[] getNearestNeighborRoute() {
+        int numLocations = distanceMatrix.length;
+        int[] route = new int[numLocations];
+
+        // Initialize the route with the starting location (0)
+        route[0] = 0;
+
+        // Keep track of which locations have been visited
+        boolean[] visited = new boolean[numLocations];
+        visited[0] = true;
+
+        // For each location in the route, find the nearest unvisited neighbor
+        for (int i = 1; i < numLocations; i++) {
+            int currentLocation = route[i - 1];
+            int nearestNeighbor = -1;
+            int shortestDistance = Integer.MAX_VALUE;
+
+            // Find the nearest unvisited neighbor of the current location
+            for (int j = 0; j < numLocations; j++) {
+                if (!visited[j] && distanceMatrix[currentLocation][j] < shortestDistance) {
+                    nearestNeighbor = j;
+                    shortestDistance = distanceMatrix[currentLocation][j];
+                }
+            }
+
+            // Add the nearest neighbor to the route and mark it as visited
+            route[i] = nearestNeighbor;
+            visited[nearestNeighbor] = true;
+
+            // Update the current location to be the nearest neighbor
+            currentLocation = nearestNeighbor;
+        }
+
+        return route;
+    }
+
+    // brute force algorithm
+
+    public int[] getBruteForceRoute() {
+        int numLocations = distanceMatrix.length;
+        int[] route = new int[numLocations];
+
+        // Initialize the route as the default order of locations (0, 1, 2, 3)
+        for (int i = 0; i < numLocations; i++) {
+            route[i] = i;
+        }
+
+        // Try all possible permutations of the route to find the shortest one
+        int shortestDistance = Integer.MAX_VALUE;
+        int[] shortestRoute = new int[numLocations];
+        do {
+            int distance = 0;
+            for (int i = 1; i < numLocations; i++) {
+                distance += distanceMatrix[route[i - 1]][route[i]];
+            }
+            distance += distanceMatrix[route[numLocations - 1]][route[0]]; // add distance back to start
+            if (distance < shortestDistance) {
+                shortestDistance = distance;
+                shortestRoute = Arrays.copyOf(route, numLocations);
+            }
+        } while (nextPermutation(route));
+
+        return shortestRoute;
+    }
+
+    private boolean nextPermutation(int[] arr) {
+        // Find the largest index k such that a[k] < a[k + 1]. If no such index exists,
+        // the permutation is the last permutation.
+        int k = arr.length - 2;
+        while (k >= 0 && arr[k] >= arr[k + 1]) {
+            k--;
+        }
+        if (k < 0) {
+            return false;
+        }
+
+        // Find the largest index l greater than k such that a[k] < a[l].
+        int l = arr.length - 1;
+        while (arr[k] >= arr[l]) {
+            l--;
+        }
+
+        // Swap the value of a[k] with that of a[l].
+        int temp = arr[k];
+        arr[k] = arr[l];
+        arr[l] = temp;
+
+        // Reverse the sequence from a[k + 1] up to and including the final element
+        // a[n].
+        int i = k + 1;
+        int j = arr.length - 1;
+        while (i < j) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    // farthest insertion algorithm
+
+    public int[] getFarthestInsertionRoute() {
+        int numLocations = distanceMatrix.length;
+        int[] route = new int[numLocations];
+
+        // Initialize the route with the starting location (0)
+        route[0] = 0;
+
+        // Keep track of which locations have been visited
+        boolean[] visited = new boolean[numLocations];
+        visited[0] = true;
+
+        // Initialize the list of unvisited locations
+        List<Integer> unvisited = new ArrayList<Integer>();
+        for (int i = 1; i < numLocations; i++) {
+            unvisited.add(i);
+        }
+
+        // For each location in the route, find the farthest unvisited neighbor
+        for (int i = 1; i < numLocations; i++) {
+            int farthestLocation = -1;
+            int farthestDistance = -1;
+
+            // Find the farthest unvisited location from the current route
+            for (int j = 0; j < i; j++) {
+                int currentLocation = route[j];
+                for (int k : unvisited) {
+                    int distance = distanceMatrix[currentLocation][k];
+                    if (distance > farthestDistance) {
+                        farthestLocation = k;
+                        farthestDistance = distance;
+                    }
+                }
+            }
+
+            // Find the location in the route where the farthest location can be inserted
+            int insertIndex = -1;
+            int shortestIncrease = Integer.MAX_VALUE;
+            for (int j = 1; j < i; j++) {
+                int location1 = route[j - 1];
+                int location2 = route[j];
+                int increase = distanceMatrix[location1][farthestLocation] + distanceMatrix[farthestLocation][location2]
+                        - distanceMatrix[location1][location2];
+                if (increase < shortestIncrease) {
+                    insertIndex = j;
+                    shortestIncrease = increase;
+                }
+            }
+
+            // Insert the farthest location into the route
+            if (insertIndex == -1) {
+                route[i] = farthestLocation;
+            } else {
+                for (int j = i; j > insertIndex; j--) {
+                    route[j] = route[j - 1];
+                }
+                route[insertIndex] = farthestLocation;
+            }
+
+            // Mark the farthest location as visited and remove it from the unvisited list
+            visited[farthestLocation] = true;
+            unvisited.remove(new Integer(farthestLocation));
+        }
+
+        return route;
     }
 }
